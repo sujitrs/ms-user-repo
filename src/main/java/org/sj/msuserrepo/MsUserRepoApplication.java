63c95137851563bf926a7b0132788301048260d5
sujitrs <suryawanshisujeet@gmail.com>
2018-12-21 15:25:47 +0530

    first commit

---
+++ src/main/java/org/sj/msuserrepo/MsUserRepoApplication.java
@@ -0,0 +1,14 @@
+package org.sj.msuserrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUserRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUserRepoApplication.class, args);
+	}
+
+}
+
