998b08bfa39716c6c86f487cd624ed6646d39236
sujitrs <suryawanshisujeet@gmail.com>
2019-02-08 17:59:29 +0530

    working working

--- src/main/java/org/sj/msuserrepo/MsUserRepoApplication.java
+++ src/main/java/org/sj/msuserrepo/MsUserRepoApplication.java
@@ -1,11 +1,15 @@
 package org.sj.msuserrepo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class MsUserRepoApplication {
 
 	public static void main(String[] args) {
